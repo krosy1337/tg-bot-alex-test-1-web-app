12475e7aac57095afd715db03e4cd8e0f14b4642
Alexey Egorov <krosy1337@gmail.com>
2022-10-25 19:32:02 +0300

    Fix fetch products

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             queryId,
         }
 
-        fetch("https://tg-bot-alex-test-1.herokuapp.com", {
+        fetch("https://tg-bot-alex-test-1.herokuapp.com/web-data", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
