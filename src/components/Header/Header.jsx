88524ae113f8b468d7c3d79310e8ca9e4429a042
Alexey Egorov <krosy1337@gmail.com>
2022-10-25 15:08:04 +0300

    Fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <div className={"header"}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={"username"}>{tg.initDataUnsafe?.user?.username}</span>
+            <span className={"username"}>{user.username}</span>
         </div>
     )
 }
